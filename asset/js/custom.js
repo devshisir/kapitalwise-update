10b6876467cbe24dcf74286dd1ead0669d8bdc45
devshisir <contact.shisirimran@gmail.com>
2022-06-03 02:00:30 +0600

    Mobile responsive done

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
+
+    // mobile menu toggle
+    $('.mobileMenuToggle button').on('click',function(){
+        $('#mobileMenuContent').toggleClass('toggle');
+    })
+    $('.menuClose').on('click',function(){
+        $('#mobileMenuContent').toggleClass('toggle');
+    })
+
+
     // testmonial slider active
     $('.testmonial__slider__active').owlCarousel({
         loop: true,
         margin: 0,
         nav: false,
         autoplay: true,
-        autoplayTimeout: 7000,
         smartSpeed: 1000,
         responsive: {
             0: {
@@ -20,47 +29,6 @@ $(document).ready(function () {
         }
     })
 
-    // pricing__banner__slider__active
-    // $('.pricing__banner__slider__active').owlCarousel({
-    //     loop: true,
-    //     margin: 0,
-    //     nav: false,
-    //     dots: false,
-    //     autoplay: true,
-    //     autoplayTimeout: 7000,
-    //     // smartSpeed: 1000,
-    //     animateIn: 'fadeIn',
-    //     animateOut: 'fadeOut',
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         600: {
-    //             items: 1
-    //         },
-    //         1000: {
-    //             items: 1
-    //         }
-    //     }
-    // })
-
-    // activate input range slider
-    const slider = document.querySelector('.slider');
-    const sliderThumb = document.querySelector('.slider__thumb');
-    const TolTipSlider = document.querySelector('.toltip__range');
-    const SliderProgress = document.querySelector('.rangeProgress');
-    const toltipValue = document.querySelector('.toltipValue');
-
-    slider.addEventListener('input',customRangeSlider);
-    function customRangeSlider(){
-        const valueMain = (slider.value);
-        const value = (slider.value / slider.max) * 100 + '%';
-        toltipValue.innerHTML = valueMain;
-        SliderProgress.style.width = value;
-        sliderThumb.style.cssText = `left: ${value}; transform: translate(${value},-50%);`;
-    }
-
-    customRangeSlider();
 
     // calencer js active
     // script link https://www.jqueryscript.net/demo/powerful-calendar/
